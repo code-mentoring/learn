55020539dbf2414f5961b50b6729fe5f3cca4eb5
Tristan Matthias <hello@tristanmatthias.com>
2020-04-18 01:56:11 -0400

    fix(api): prod db url config

--- packages/api/src/Database.module.ts
+++ packages/api/src/Database.module.ts
@@ -3,7 +3,7 @@ import config from 'config';
 import path from 'path';
 
 const db = process.env.NODE_ENV === 'production'
-  ? { uri: process.env.DATABASE_URL}
+  ? { url: process.env.DATABASE_URL }
   : config.get('db') as {}
 
 
