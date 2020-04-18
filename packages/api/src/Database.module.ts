ae6becabcb8159a7e446e11bac654569417e1cae
Tristan Matthias <hello@tristanmatthias.com>
2020-04-18 01:24:09 -0400

    feat(api): added prod db settings

--- packages/api/src/Database.module.ts
+++ packages/api/src/Database.module.ts
@@ -1,11 +1,15 @@
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeOrmModule } from '@nestjs/typeorm';
 import config from 'config';
 import path from 'path';
+
+const db = process.env.NODE_ENV === 'production'
+  ? { uri: process.env.DATABASE_URL}
+  : config.get('db') as {}
 
 
 export const DatabaseModule = TypeOrmModule.forRoot({
 
-  ...config.get('db'),
+  ...db,
 
   entities: [path.join(__dirname, '**/**.entity{.ts,.js}')],
   synchronize: true,
