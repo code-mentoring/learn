f8a25a1fd2fb60d2c2e90654a929e0f7dae3e7ac
Tristan Matthias <hello@tristanmatthias.com>
2020-04-18 02:25:11 -0400

    fix(client): fix api url

--- packages/client/webpack.config.ts
+++ packages/client/webpack.config.ts
@@ -94,7 +94,7 @@ const config: Configuration = {
     }),
     new replacePlugin({
       values: {
-        '%%API_HOST%%': isProd ? 'https://learn.codementoring.co' : 'http://localhost:4000',
+        '%%API_HOST%%': isProd ? 'http://api.codementoring.co' : 'http://localhost:4000',
         '%%IS_PROD%%': isProd
       }
     })
