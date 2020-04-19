35a36ba2a6f3f2c60718296063b7fdb1ca1ab524
Tristan Matthias <hello@tristanmatthias.com>
2020-04-18 22:12:46 -0400

    style(client): styled login form

--- packages/client/src/pages/Login/Login.page.tsx
+++ packages/client/src/pages/Login/Login.page.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 import * as yup from 'yup';
 
 import { Page } from '../../components/Page/Page';
+import { Auth } from '../../containers/Auth.container';
 import Logo from '../../images/logo.svg';
 import { getGQLError } from '../../lib/apollo';
-import { Auth } from '../../containers/Auth.container';
+
 
 
 const loginValidation = yup.object().shape({
@@ -15,18 +16,18 @@ const loginValidation = yup.object().shape({
 
 
 export const LoginPage = () => {
-  const { login, loginError, loginLoading } = Auth.useContainer();
+  const { login, loginError } = Auth.useContainer();
   const submit = (e: { email: string, password: string }) => {
     login(e.email, e.password);
   };
 
-  return <Page title="Login" type="login" className="grid items-center justify-center bg-grey-100 text-center">
-    <Card className="center bg-white">
-      <Logo className="logo h-12 inline m-6 mb-10" />
+  return <Page title="Login" type="login" className="grid items-center justify-center bg-blue-500 text-center">
+    <Card className="center bg-white w-64" padding={4}>
+      <Logo className="logo h-12 inline m-6" />
       <Form onSubmit={submit} error={getGQLError(loginError)} validationSchema={loginValidation}>
-        <FormField label="email" name="email" type="text" placeholder="Email" icon="user" iconColor="grey-40" />
-        <FormField label="password" name="password" placeholder="Password" type="password" icon="lock" iconColor="grey-40" />
-        <Button disabled={loginLoading}>Login</Button>
+        <FormField name="email" type="text" placeholder="Email" icon="user" iconColor="grey-40" />
+        <FormField name="password" placeholder="Password" type="password" icon="lock" iconColor="grey-40" />
+        <Button>Login</Button>
       </Form>
     </Card>
   </Page>;
