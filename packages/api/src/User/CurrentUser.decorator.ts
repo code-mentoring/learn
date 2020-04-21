7e6544b39c8ff375d4763f54605c1e8f8a8d8efe
Quim Granados <quimgranados@gmail.com>
2020-04-21 12:04:43 +1000

    fix: jwt payload

--- packages/api/src/User/CurrentUser.decorator.ts
+++ packages/api/src/User/CurrentUser.decorator.ts
@@ -1,19 +1,7 @@
 import { createParamDecorator } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
-import decode from 'jwt-decode';
-
-import { User } from './User.entity';
 
 export const CurrentUser = createParamDecorator((_data, context) => {
   const ctx = GqlExecutionContext.create(context);
-  const authHeader = ctx.getContext().req.headers.authorization;
-  const token = authHeader.split(' ')[1];
-  if (!token) return null;
-  const { user }: { user: User } = decode(token);
-  return {
-    id: user.id,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    email: user.email,
-  };
+  return ctx.getContext().req.user;
 });
