c8988fb78af5cca7d3c52d61f70a96d4a04aed28
Quim Granados <quimgranados@gmail.com>
2020-04-25 15:54:07 +1000

    fix: db clean and user email/pass seeder

--- packages/api/src/Database/seeders/entities.ts
+++ packages/api/src/Database/seeders/entities.ts
@@ -2,11 +2,17 @@ import faker from 'faker';
 
 import { UserService } from '../../User/User.service';
 
-export const seedUsers = async (userService: UserService) => userService.create({
-  firstName: faker.name.firstName(),
-  lastName: faker.name.lastName(),
-  email: 'test@test.com',
-  password: 'test123'
-});
+export const seedUsers = async (userService: UserService) => Promise.all(Array(5).fill(undefined).map(async (_, i) => {
+  const firstName = faker.name.firstName();
+  const lastName = faker.name.lastName();
+
+  const user = await userService.create({
+    firstName,
+    lastName,
+    email: `user${i}@test.com`,
+    password: `user${i}123`
+  });
+  return user;
+}));
 
 // seed paths, lessons...
--- packages/api/src/Database/seeders/index.ts
+++ packages/api/src/Database/seeders/index.ts
@@ -4,20 +4,30 @@ import Listr from 'listr';
 import { AppModule } from '../../App.module';
 import { UserService } from '../../User/User.service';
 import { seedUsers } from './entities';
+import { DatabaseService } from '../Database.service';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
 
   const userService = app.get(UserService);
+  const dataBaseService = app.get(DatabaseService);
 
   (async () => {
     await new Listr([
       {
-        title: 'Create users',
-        task: async () => seedUsers(userService)
+        title: 'Reset DB',
+        task: async () => {
+          dataBaseService.DANGEROUSLY_RESET_DATABASE();
+        }
       },
       {
-        title: 'Closing App',
+        title: 'Create users',
+        task: async () => {
+          await seedUsers(userService);
+        }
+      },
+      {
+        title: 'Close App',
         task: async () => app.close()
       }
     ]).run();
