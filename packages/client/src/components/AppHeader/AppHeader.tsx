b7c719258f1b767fe7ffc95fe20aaf4561f3ed56
Suzy <suzy.nakayama@gmail.com>
2020-04-26 00:45:54 -0400

    fix(client): App Header: indentations

--- packages/client/src/components/AppHeader/AppHeader.tsx
+++ packages/client/src/components/AppHeader/AppHeader.tsx
@@ -16,7 +16,7 @@ import './AppHeader.css';
 // TODO: Replace user profile picture when #27 is completed.
 // TODO: Replace the class icon with the actual class the student is
 // TODO: Fix points when the api is done (#26 ?)
-// TODO: Add conditionals for things that are supposed to appear 
+// TODO: Add conditionals for things that are supposed to appear
 //          depending on the page(ie.points, course)
 
 export const AppHeader: React.FC = () => {
@@ -33,7 +33,7 @@ export const AppHeader: React.FC = () => {
   const handleHover = (where: string, bool: boolean) => {
     if (where === 'menu') {
       setMenu(bool);
-      setHover(bool)
+      setHover(bool);
     } else {
       setPath(bool);
       setHover(bool);
@@ -41,64 +41,64 @@ export const AppHeader: React.FC = () => {
   };
 
   return (
-		<nav className="border-b-2 border-solid border-grey-500 p-4">
-			<Link to="/dashboard">
-				<LogoMark className="logo h-10 inline ml-20" />
-			</Link>
+    <nav className="border-b-2 border-solid border-grey-500 p-4">
+      <Link to="/dashboard">
+        <LogoMark className="logo h-10 inline ml-20" />
+      </Link>
 
-			<div className="inline text-grey-700 font-bold text-sm uppercase">
-				<Link to="/friends" className="mx-3">
-					Friends
-				</Link>
-			</div>
+      <div className="inline text-grey-700 font-bold text-sm uppercase">
+        <Link to="/friends" className="mx-3">
+          Friends
+        </Link>
+      </div>
 
-			<div className="inline font-semibold float-right mr-16">
-				<PathIcon
+      <div className="inline font-semibold float-right mr-16">
+        <PathIcon
           icon="js"
           size="medium"
           className="inline cursor-pointer"
-					onMouseEnter={() => handleHover("paths", true)}
-					onMouseLeave={() => handleHover("paths", false)}
-				/>
-				{path && hover && (
-					<DropdownMenu
-						title="My Paths"
-						subjects={paths}
-						data={null}
-						className={`absolute bg-white flex flex-col paths`}
-						onMouseEnter={() => handleHover("paths", true)}
-						onMouseLeave={() => handleHover("paths", false)}
-					/>
-				)}
+          onMouseEnter={() => handleHover("paths", true)}
+          onMouseLeave={() => handleHover("paths", false)}
+        />
+        {path && hover && (
+          <DropdownMenu
+            title="My Paths"
+            subjects={paths}
+            data={null}
+            className={`absolute bg-white flex flex-col paths`}
+            onMouseEnter={() => handleHover("paths", true)}
+            onMouseLeave={() => handleHover("paths", false)}
+          />
+        )}
 
-				<div className="text-tertiary-500 inline ml-8 mr-8">
-					<span className="text-lg">5</span>
-					<Icon icon="fire" size="small" className="inline ml-1 mb-1" />
-				</div>
+        <div className="text-tertiary-500 inline ml-8 mr-8">
+          <span className="text-lg">5</span>
+          <Icon icon="fire" size="small" className="inline ml-1 mb-1" />
+        </div>
 
-				<button
-					type="button"
-					onMouseEnter={() => handleHover("menu", true)}
-					onMouseLeave={() => handleHover("menu", false)}
-				>
-					<img
-						className="rounded-circle inline-block cursor-pointer"
-						src="http://placehold.it/30x30"
-						alt="User profile pic"
-					/>
-				</button>
-				{menu && hover && (
-					<DropdownMenu
-						title="My Account"
-						subjects={menuItems}
-						data={me}
-						className={`absolute top-20 left-20 bg-white flex flex-col menu`}
-						onMouseEnter={() => handleHover("menu", true)}
-						onMouseLeave={() => handleHover("menu", false)}
-					/>
-				)}
-			</div>
-		</nav>
-	);
+        <button
+          type="button"
+          onMouseEnter={() => handleHover("menu", true)}
+          onMouseLeave={() => handleHover("menu", false)}
+        >
+          <img
+            className="rounded-circle inline-block cursor-pointer"
+            src="http://placehold.it/30x30"
+            alt="User profile pic"
+          />
+        </button>
+        {menu && hover && (
+          <DropdownMenu
+            title="My Account"
+            subjects={menuItems}
+            data={me}
+            className={`absolute top-20 left-20 bg-white flex flex-col menu`}
+            onMouseEnter={() => handleHover("menu", true)}
+            onMouseLeave={() => handleHover("menu", false)}
+          />
+        )}
+      </div>
+    </nav>
+  );
 };
 
