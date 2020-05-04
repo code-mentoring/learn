9e822b75b5355e4601bdf12875f69d503cb18136
Tristan Matthias <hello@tristanmatthias.com>
2020-05-04 01:08:15 -0400

    ci: remove branchname

--- lib/publish.ts
+++ lib/publish.ts
@@ -1,7 +1,5 @@
 import execa from 'execa';
 
-import { formattedBranchName } from './branchName';
-
 
 (async () => {
   const [,, npmToken = process.env.NPM_TOKEN] = process.argv;
@@ -10,7 +8,7 @@ import { formattedBranchName } from './branchName';
   const isCI = Boolean(process.env.CI);
   if (!isCI) throw new Error('Do not run this command in a non CI environment');
 
-  const branch = process.env.BRANCH || await formattedBranchName();
+  const branch = process.env.BRANCH;
   console.log(`Publishing to ${branch}`);
 
   const subprocess = execa('yarn', [
@@ -19,7 +17,7 @@ import { formattedBranchName } from './branchName';
     '-y',
     '--canary',
     `--preid=${branch}`,
-    `--npm-tag=${branch}`,
+    `--dist-tag=${branch}`,
     '--force-publish=*',
     `--registry="https://npm.pkg.github.com/:_authToken=${npmToken}"`
   ]);
