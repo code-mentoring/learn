0dbbb04d1b84ac00feb4282e4b2330e75d678314
Tristan Matthias <hello@tristanmatthias.com>
2020-05-04 00:36:17 -0400

    ci: added publish script

---
+++ lib/publish.ts
@@ -0,0 +1,27 @@
+import execa from 'execa';
+import { formattedBranchName } from './branchName';
+
+
+(async () => {
+  const [,, npmToken = process.env.NPM_TOKEN] = process.argv;
+  if (!npmToken) throw new Error('No NPM_TOKEN configured');
+
+  const isCI = Boolean(process.env.CI);
+  if (!isCI) throw new Error('Do not run this command in a non CI environment');
+
+  const branch = await formattedBranchName();
+  console.log(`Publishing to ${branch}`);
+
+  const { stdout } = await execa('yarn', [
+    'lerna',
+    'publish',
+    '-y',
+    '--canary',
+    `--preid=${branch}`,
+    `--npm-tag=${branch}`,
+    '--force-publish=*',
+    `--registry="https://npm.pkg.github.com/:_authToken=${npmToken}"`
+  ]);
+  console.log(stdout);
+
+})();
