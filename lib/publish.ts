125f5f64f5b651e1949104dd28d3d075de7f190a
Tristan Matthias <hello@tristanmatthias.com>
2020-05-05 15:37:10 -0400

    ci: added new workflows

---
+++ lib/publish.ts
@@ -0,0 +1,54 @@
+import execa from 'execa';
+
+
+(async () => {
+  const [,,
+    branch = process.env.BRANCH
+  ] = process.argv;
+
+  if (!branch) throw new Error('No branch supplied!');
+  const isCI = Boolean(process.env.CI);
+  if (!isCI) throw new Error('Do not run this command in a non CI environment');
+
+  let tag = branch.replace(/[/\s]/g, '-');
+  let bump;
+  let extraFlags: string[] = [];
+
+  switch (branch) {
+    case 'master':
+      tag = 'stable';
+      bump = 'minor';
+      break;
+
+    case 'develop':
+      tag = 'beta';
+      bump = 'patch';
+      break;
+
+    default:
+      if (!branch.startsWith('feature/')) throw new Error(`Unknown branch ${branch}`);
+      tag = branch.replace(/[/\s]/g, '-');
+      bump = 'prerelease';
+      extraFlags = extraFlags.concat(`--preid=${tag}`);
+  }
+
+  console.log(`Publishing to tag: ${tag}`);
+
+  const args = [
+    'lerna',
+    'publish',
+    bump,
+    '-y',
+    `--dist-tag=${tag}`,
+    '--force-publish=*',
+    ...extraFlags
+  ].filter(f => f) as string[];
+
+  const subprocess = execa('yarn', args);
+
+  subprocess.stdout!.pipe(process.stdout);
+
+  const { exitCode } = await subprocess;
+  if (exitCode) process.exit(exitCode);
+
+})();
