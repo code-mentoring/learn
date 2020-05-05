c3771e0c2684b399c800f10751e9da1ce387fcae
Tristan Matthias <hello@tristanmatthias.com>
2020-05-05 12:00:15 -0400

    ci: added branch param to publish

--- lib/publish.ts
+++ lib/publish.ts
@@ -2,23 +2,39 @@ import execa from 'execa';
 
 
 (async () => {
-  const [,, npmToken = process.env.NPM_TOKEN] = process.argv;
+  const [,,
+    branch = process.env.BRANCH,
+    npmToken = process.env.NPM_TOKEN
+  ] = process.argv;
   if (!npmToken) throw new Error('No NPM_TOKEN configured');
 
+  if (!branch) throw new Error('No branch supplied!');
   const isCI = Boolean(process.env.CI);
   if (!isCI) throw new Error('Do not run this command in a non CI environment');
 
-  let branch = process.env.BRANCH!;
-  branch = branch.replace(/[/\s]/g, '-');
-  console.log(`Publishing to ${branch}`);
+  let tag = branch.replace(/[/\s]/g, '-');
+  switch (branch) {
+    case 'master':
+      tag = 'stable';
+      break;
+    case 'develop':
+      tag = 'beta';
+      break;
+    default:
+      if (branch.startsWith('feature/')) {
+        tag = branch.replace(/[/\s]/g, '-');
+      } else throw new Error(`Unknown branch ${branch}`);
+  }
+
+  console.log(`Publishing to tag: ${tag}`);
 
   const subprocess = execa('yarn', [
     'lerna',
     'publish',
     'prepatch',
     '-y',
-    `--preid=${branch}`,
-    `--dist-tag=${branch}`,
+    `--preid=${tag}`,
+    `--dist-tag=${tag}`,
     '--force-publish=*',
     `--registry="https://npm.pkg.github.com/:_authToken=${npmToken}"`
   ]);
