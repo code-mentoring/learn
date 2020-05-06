4fb355eb447950a9feb2135b1250d4b036c35247
Ellie <>
2020-05-06 02:37:14 -0400

    feat(client): remove temp test code to restore Dashboard.page.tsx

--- packages/client/src/pages/Dashboard/Dashboard.page.tsx
+++ packages/client/src/pages/Dashboard/Dashboard.page.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Page } from '../../components/Page/Page';
-// import { LeaderboardWidget } from '../../components/LeaderBoardWidget/LeaderBoardWidget';
-import { PathModule } from '../../components/PathModule/PathModule';
+import { LeaderboardWidget } from '../../components/LeaderBoardWidget/LeaderBoardWidget';
 
 export const DashboardPage = () => (
   <Page title="Dashboard" type="dashboard" className="bg-white">
     <h1>Dashboard</h1>
-    {/* <LeaderboardWidget className="w-64" /> */}
-    <PathModule />
+    <LeaderboardWidget className="w-64" />
   </Page>
 );
