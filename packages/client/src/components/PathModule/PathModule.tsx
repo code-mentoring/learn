f2f4c36eca1c3fc2ec6568f5a458d9828d2b6184
Quim Granados <quimgranados@gmail.com>
2020-05-06 15:35:37 +1000

    fix: add module css

--- packages/client/src/components/PathModule/PathModule.tsx
+++ packages/client/src/components/PathModule/PathModule.tsx
@@ -1,30 +1,29 @@
-import './PathModule.css';
-
 import React from 'react';
 import classnames from 'classnames';
 import { Hexagon } from './Icons/hexagon';
 import { CheckMark } from './Icons/checkMark';
+import styles from './PathModule.module.css';
 
 const mockModule = {
   id: 2,
   name: 'Intro to JS',
-  completed: false,
+  completed: true,
   previous: {
     id: 1,
-    completed: true
+    completed: false
   }
 };
 
 export const PathModule : React.FunctionComponent = () => {
   const statusClass = classnames({
-    completed: mockModule.completed,
-    open: !mockModule.completed && mockModule.previous.completed,
-    locked: !mockModule.completed && !mockModule.previous.completed
+    [`${styles.completed}`]: mockModule.completed,
+    [`${styles.open}`]: !mockModule.completed && mockModule.previous.completed,
+    [`${styles.locked}`]: !mockModule.completed && !mockModule.previous.completed
   });
   return <div className="relative w-24 flex flex-col items-center">
     <Hexagon classname={statusClass} />
     {mockModule.completed
-        && <CheckMark classname="absolute checkmark" />}
+        && <CheckMark classname={`absolute ${styles.checkmark}`} />}
     <div className={classnames('name', statusClass)}>
       {mockModule.name}
     </div>
