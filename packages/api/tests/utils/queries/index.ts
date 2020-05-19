f7a1c90be987343b0b69ac1ed23077de01a0f1d4
Tristan Matthias <hello@tristanmatthias.com>
2020-05-18 20:23:09 -0400

    test(api): improved importing of gql files

--- packages/api/tests/utils/queries/index.ts
+++ packages/api/tests/utils/queries/index.ts
@@ -1,10 +1,15 @@
-
 import fs from 'fs';
 import path from 'path';
 
-export default {
-  getPathByName: fs.readFileSync(path.join(__dirname, './getPathByName.gql')).toString(),
-  me: fs.readFileSync(path.join(__dirname, './me.gql')).toString(),
-  modules: fs.readFileSync(path.join(__dirname, './modules.gql')).toString(),
-  getUserFriends: fs.readFileSync(path.join(__dirname, './getUserFriends.gql')).toString()
-};
+const files = [
+  'getPathByName',
+  'me',
+  'modules',
+  'getUserFriends'
+];
+
+export default files.reduce((obj, file) => {
+  // eslint-disable-next-line no-param-reassign
+  obj[file] = fs.readFileSync(path.join(__dirname, `./${file}.gql`)).toString();
+  return obj;
+}, {} as {[key: string]: string});
