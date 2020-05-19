57f3702f0db863a0cb1b5eb831a8558e796b3af6
Weiwei <ww.zhang@mail.utoronto.ca>
2020-05-19 19:59:01 -0400

    modification for Lint rules

--- packages/client/src/components/SignOutBtn/SignOutBtn.tsx
+++ packages/client/src/components/SignOutBtn/SignOutBtn.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
-import {Auth} from '../../containers/Auth.container';
-import classes from './SignOutBtn.module.css'
+import { Auth } from '../../containers/Auth.container';
+import classes from './SignOutBtn.module.css';
 
-export const SignOutBtn = () =>{
-    const { signOut } = Auth.useContainer();
-    return(
-        <button
-            className={classes.Btn}
-            type="button"
-            onClick ={()=>{
-                signOut();
-                alert("You have signed out");
-            }}
-        >
-            Sign out
-        </button>
-    )
-}
+export const SignOutBtn = () => {
+  const { signOut } = Auth.useContainer();
+  return (
+      <button
+          className = {classes.Btn}
+          type = "button"
+          onClick = {() => {
+              signOut();
+              console.log('You have signed out');
+          }}
+      >
+          Sign out
+      </button>
+  );
+};
