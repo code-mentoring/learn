3f5671270e309f3ae8ea73cd5039f352be6e5351
Weiwei <ww.zhang@mail.utoronto.ca>
2020-05-28 11:20:39 -0400

    remove the extra css file

--- packages/client/src/components/SignOutBtn/SignOutBtn.tsx
+++ packages/client/src/components/SignOutBtn/SignOutBtn.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Auth } from '../../containers/Auth.container';
-import {Button} from '../../../../ui/components/Button/Button'
+import { Button } from '../../../../ui/components/Button/Button'
 
 export const SignOutBtn = () => {
   const { signOut } = Auth.useContainer();
