70641d3f0f5b9ff08dc3d799d0c0f28e085ddc18
Tristan Matthias <hello@tristanmatthias.com>
2020-05-28 15:10:53 -0400

    feat(ui): added disabled prop to button

--- packages/ui/components/Button/Button.tsx
+++ packages/ui/components/Button/Button.tsx
@@ -22,8 +22,6 @@ export const Button: React.FunctionComponent<ButtonProps & Omit<React.ButtonHTML
   if (size) classes[`btn-${size}`] = true;
   if (text) classes['btn-text'] = true;
 
-  // TODO: disabled styles
-
   // eslint-disable-next-line react/button-has-type
   return <button
     type={type}
