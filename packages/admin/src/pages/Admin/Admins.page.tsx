d3d7c1e2b77975c2a444edce173a1646c83e106e
Ellie <>
2020-05-30 13:33:46 -0400

    feat: optimise the grid expression

--- packages/admin/src/pages/Admin/Admins.page.tsx
+++ packages/admin/src/pages/Admin/Admins.page.tsx
@@ -28,21 +28,28 @@ export const AdminsPage = () => {
   return <>
     <Page title="Admin" className="bg-grey-100" styles={{ display: 'grid', gridTemplateColumns: '180px auto', gridTemplateRows: '80px auto' }}>
       <PageContent styles={{ position: 'relative', margin: '0 2rem 2rem' }}>
-        <div className=" grid grid-cols-12 uppercase text-grey-500 text-xs font-extrabold tracking-wider pb-2 border-b border-grey-200">
-          <div className="col-start-1 col-end-2">type</div>
-          <div className="col-start-2 col-end-5">name</div>
-          <div className="col-start-5 col-end-12">email</div>
-          <div className="col-start-12 col-end-12 text-right">actions</div>
+        <div
+          className="uppercase text-grey-500 text-xs font-extrabold tracking-wider pb-2 border-b border-grey-200"
+          style={{ display: 'grid', gridTemplateColumns: '1fr 3fr 6fr 1fr' }}
+        >
+          <div>type</div>
+          <div>name</div>
+          <div>email</div>
+          <div className="text-right">actions</div>
         </div>
+
         { adminList.map(admin =>
-          <div className="grid grid-cols-12 text-grey-800 text-sm py-2 border-b border-grey-200">
+          <div
+            className="text-grey-800 text-sm py-2 border-b border-grey-200"
+            style={{ display: 'grid', gridTemplateColumns: '1fr 3fr 6fr 1fr' }}
+          >
             {/* TODO: icon to be determined by admin type */}
-            <div className="col-start-1 col-end-2"><Icon icon="shieldAlt" size="small" color="pink-500" /></div>
-            <div className="col-start-2 col-end-5"> { admin.firstName } { admin.lastName }</div>
-            <div className="col-start-5 col-end-12">{ admin.email }</div>
+            <div><Icon icon="shieldAlt" size="small" color="pink-500" /></div>
+            <div> { admin.firstName } { admin.lastName }</div>
+            <div>{ admin.email }</div>
             {/* TODO: action to be complete after design */}
-            <div className="col-start-12 col-end-12 content-end"><Icon icon="user" size="small" color="secondary-500" /></div>
-          </div>)}
+            <div className="content-end"><Icon icon="user" size="small" color="secondary-500" /></div>
+          </div>) }
       </PageContent>
     </Page>
   </>;
