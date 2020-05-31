e0551e4c1749a1bcaadbbbc6ba44c21172ed5926
Tristan Matthias <hello@tristanmatthias.com>
2020-05-31 15:38:05 -0400

    feat(client): added module tree

---
+++ packages/client/src/containers/Path.container.ts
@@ -0,0 +1,50 @@
+import { useQuery, useLazyQuery } from '@apollo/react-hooks';
+import { Query, QueryPathArgs } from '@codement/api';
+import { useState, useEffect } from 'react';
+import { createContainer } from 'unstated-next';
+
+import myPathsQuery from '../gql/queries/myPaths.gql';
+import pathQuery from '../gql/queries/pathWithModules.gql';
+
+
+const LS_PATH = 'path';
+const initialPath = localStorage.getItem(LS_PATH);
+
+
+export const Path = createContainer(() => {
+  const [currentPathId, setCurrentPathId] = useState<string | null>(initialPath);
+  const { data: myPaths } = useQuery<{ myPaths: Query['myPaths'] }>(myPathsQuery);
+
+  // Fetch BOTH the path AND it's modules
+  const [fetchPathWithModules, { data: pathData }] = useLazyQuery<{
+    path: Query['path'],
+    pathModules: Query['pathModules']
+  }, QueryPathArgs>(pathQuery);
+
+
+  // First run, load paths if no initial path
+  useEffect(() => {
+    if (!currentPathId && myPaths?.myPaths) setCurrentPathId(myPaths.myPaths[0].id);
+  }, [myPaths]);
+
+
+  // Update local storage LS_PATH when the current path changes
+  useEffect(() => {
+    if (currentPathId) localStorage.setItem(LS_PATH, currentPathId);
+    else localStorage.removeItem(LS_PATH);
+
+    if (currentPathId) {
+      fetchPathWithModules({
+        variables: { id: currentPathId }
+      });
+    }
+  }, [currentPathId]);
+
+  return {
+    currentPathId,
+    setCurrentPathId,
+    currentPath: pathData?.path,
+    currentModules: pathData?.pathModules,
+    myPaths: myPaths?.myPaths
+  };
+});
