87bcf2f7f28aa5d7c75dac0bec4da88d0bddd145
Rajat Bansal <rjtbansal@gmail.com>
2020-05-30 23:27:56 -0400

    feat(client): restyled login page

--- packages/client/src/pages/Login/Login.page.tsx
+++ packages/client/src/pages/Login/Login.page.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Form, FormField } from '@codement/ui';
+import { Button, Form, FormField, Checkbox } from '@codement/ui';
 import React from 'react';
 import * as yup from 'yup';
 
@@ -6,7 +6,7 @@ import { Auth } from '@codement/ui/lib/containers/Auth.container';
 import { getGQLError } from '@codement/ui/lib/apollo';
 import Logo from '@codement/ui/images/logo.svg';
 import { Page } from '../../components/Page/Page';
-
+import People from '@codement/ui/images/welcome-people.svg';
 
 const loginValidation = yup.object().shape({
   email: yup.string().email().required(),
@@ -20,14 +20,25 @@ export const LoginPage = () => {
     login(e.email, e.password);
   };
 
-  return <Page title="Login" type="login" header={false} className="grid items-center justify-center bg-blue-500 text-center">
-    <Card className="center bg-white w-64" padding={4}>
+  return <Page title="Login" type="login" header={false} className="bg-white text-center">
       <Logo className="logo h-12 inline m-6" />
-      <Form onSubmit={submit} error={getGQLError(loginError)} validationSchema={loginValidation}>
-        <FormField name="email" type="text" placeholder="Email" icon="user" iconColor="grey-40" />
-        <FormField name="password" placeholder="Password" type="password" icon="lock" iconColor="grey-40" />
-        <Button>Login</Button>
-      </Form>
-    </Card>
+      <div className="mt-24">
+        <h1>Welcome back!</h1>
+        <p className="body-2 mt-2 mb-4">Lets get coding 😊</p>
+        <Form className="max-w-xs mx-auto" onSubmit={submit} error={getGQLError(loginError)} validationSchema={loginValidation}>
+          <FormField className="mb-2" name="email" type="text" placeholder="Email" />
+          <FormField className="mb-4" name="password" placeholder="Password" type="password" />
+          <div className="flex justify-between">
+            <div className="flex">
+              <Checkbox className="h-4 w-4" />
+              <p className="body-3">Remember Me</p>
+            </div>
+            <p className="body-3 underline cursor-pointer">Forgot Password?</p>
+          </div>
+          <Button color="success">Login</Button>
+        </Form>
+        <p className="body-2 mt-40 mb-4">New to Code Mentoring? <a href="https://codementoring.co/signup" className="font-extrabold text-primary-500">Join now</a></p>
+     </div>
+     <People className="fixed bottom-0 left-0 h-56" />
   </Page>;
 };
