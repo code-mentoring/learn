abcdb69161cfb6aae09b7703547e76f40e44eec5
f-westergren <folke.westergren@gmail.com>
2020-06-05 09:03:40 -0400

    feat: add some styles using styled-components

--- packages/ui/components/Button/Button.tsx
+++ packages/ui/components/Button/Button.tsx
@@ -1,14 +1,16 @@
 import classnames from 'classnames';
 import React from 'react';
-import { Size, WithColor } from '../../lib/classes';
+import styled, { css } from 'styled-components';
+import { theme } from '@codement/ui';
 
 
-export type ButtonProps = WithColor & {
+export type ButtonProps = {
   text?: boolean;
-  size?: Size;
+  large?: string;
+  success?: string;
 };
 
-export const Button: React.FunctionComponent<ButtonProps & Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'size'>> = ({
+export const CButton: React.FunctionComponent<ButtonProps & Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'size'>> = ({
   children,
   className,
   text,
@@ -24,8 +26,51 @@ export const Button: React.FunctionComponent<ButtonProps & Omit<React.ButtonHTML
   // eslint-disable-next-line react/button-has-type
   return <button
     type={type}
-    className={classnames('btn', classes, className)}
+    className={classnames(className)}
     {...props}
   >{children}
   </button>;
 };
+
+export const Button = styled(CButton)`
+  display: inline-block;
+  font-weight: 700;
+  height: 2rem;
+  line-height: 2rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  border-radius: 0.25rem;
+  letter-spacing: 0.05em;
+  font-size: 0.875rem;
+  color: ${props => props.theme.colors.white};
+  text-transform: uppercase;
+  background-color: ${props => props.theme.colors.primary['500']};
+
+  &:hover {
+    background-color: ${props => props.theme.colors.primary['400']};
+  }
+
+  &:focus {
+    background-color: ${props => props.theme.colors.primary['600']};
+  }
+
+  ${({ large }) => large && css`
+    padding-left: 1.25rem;
+    padding-right: 1.25rem;
+    height: 3rem;
+    line-height: 3rem;
+    font-size: 1rem;
+  `}
+
+  ${({ success }) => success && css`
+    background-color: ${props => props.theme.colors.secondary['500']};
+    &:hover {
+      background-color: ${props => props.theme.colors.secondary['400']};
+    }
+    &:focus {
+    background-color: ${props => props.theme.colors.secondary['600']};
+    }
+  `}
+
+  ${({ })}
+`;
