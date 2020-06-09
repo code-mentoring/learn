fb610cc18a12ba85f64eaa45209a323a76a468ff
f-westergren <folke.westergren@gmail.com>
2020-06-09 17:44:49 -0400

    fix: add icon option

--- packages/ui/components/Button/Button.tsx
+++ packages/ui/components/Button/Button.tsx
@@ -1,4 +1,6 @@
+import React, { HTMLAttributes } from 'react';
 import styled, { css } from 'styled-components';
+import { icons } from '/icons';
 
 enum BtnType {
   primary = 'primary',
@@ -7,15 +9,21 @@ enum BtnType {
   transparent = 'transparent'
 }
 
-export type ButtonProps = {
+export interface ButtonProps extends HTMLAttributes<any> {
   text?: boolean;
   success?: string;
   btnType?: 'primary' | 'secondary' | 'tertiary' | 'transparent'
   disabled?: boolean;
   size?: string;
+  icon?: boolean;
+  iconName?: string;
 };
 
-export const Button = styled.button<ButtonProps>`
+export const CButton: React.FunctionComponent<ButtonProps> = ({
+  children
+}) => <button>{children}</button>;
+
+export const StyledButton = styled.button<ButtonProps>`
   display: inline-block;
   font-weight: 800;
   height: 2.25rem;
@@ -85,3 +93,30 @@ export const Button = styled.button<ButtonProps>`
     }
   `}
 `;
+
+
+// import React from 'react';
+// import classnames from 'classnames';
+// import { Icon } from '@codement/ui';
+
+// interface LearnedConceptsProps {
+//   concepts: any[];
+//   learnedConcepts: any[];
+// }
+
+// export const LearnedConcepts: React.FC<LearnedConceptsProps> = ({
+//   concepts,
+//   learnedConcepts
+// }) => <div className="flex flex-col items-center absolute right-0 mr-6 w-20">
+//   <h4 className="text-center my-3">Learned concepts</h4>
+//   {concepts.map(concept => {
+//     const learned = learnedConcepts.map(lc => lc.id).includes(concept.id);
+//     return <div
+//       key={concept.id}
+//       className={classnames('w-10 h-10 border-2 rounded-circle mb-3 flex flex-col justify-center bg-secondary-100 text-secondary-600', {
+//         'text-grey-200 bg-white': !learned })}
+//     >
+//       {learned && <Icon icon={concept.icon} />}
+//     </div>;
+//   })}
+// </div>;
