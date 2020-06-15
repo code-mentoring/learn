ab18c15c98960057935cc6fb220818c27c4984a6
Ellie <>
2020-06-15 13:56:33 -0400

    fix(ui): remove other props

--- packages/ui/components/Icon/Icon.tsx
+++ packages/ui/components/Icon/Icon.tsx
@@ -8,26 +8,14 @@ export type IconType = keyof typeof icons;
 
 export interface IconProps extends PropsWithRef<any> {
   icon: IconType;
-  size: 'large' | 'small' | 'medium' | number;
   className: string;
-  display: string;
-  position: string;
-  left:string;
-  top:string;
-  margin: number;
-  marginTop: number;
-  marginBottom: number;
-  marginLeft: number;
-  marginRight: number;
-  transform: string;
-  height: string;
-  width: number | 'auto';
+  size: 'large' | 'small' | 'medium' | number;
   color: string;
-  fill: string;
   stroke: string;
   strokeWidth: number;
 }
 
+// in rem
 export const sizeMap = {
   small: '1',
   medium: '2.5',
@@ -38,43 +26,20 @@ export const Icon = styled<React.FC<IconProps>>(({
   icon,
   className,
   size = 'medium',
-  display,
-  position,
-  left,
-  top,
-  margin,
-  marginLeft,
-  marginRight,
-  marginTop,
-  marginBottom,
-  transform,
-  height,
-  width = 'auto',
   color,
-  fill,
   stroke,
   strokeWidth,
   ...props
 }:IconProps) => {
-  const klass = classnames('icon', className);
+  const klass = classnames('icon', icon, className);
   const Ikon = icons[icon];
   if (!Ikon) return null;
   return <Ikon {...props} className={klass} />;
 })`
-display:${({ display }) => display};
-position:${({ position }) => position};
-left:${({ left }) => left};
-top:${({ top }) => top};
-margin:${({ margin }) => margin && `${margin}rem`};
-margin-left:${({ marginLeft }) => marginLeft && `${marginLeft}rem`};
-margin-right:${({ marginRight }) => marginRight && `${marginRight}rem`};
-margin-top:${({ marginTop }) => marginTop && `${marginTop}rem`};
-margin-bottom:${({ marginBottom }) => marginBottom && `${marginBottom}rem`};
-transform:${({ transform }) => transform};
 height:${({ size }) =>`${!size ? sizeMap['medium'] : typeof size === 'number' ? size : sizeMap[size]}rem`};
-width:${({ width }) => typeof width === 'number' ? `${width}rem` : 'auto'};
+width:auto;
 color:${({ color }) => color};
-fill:${({ fill }) => fill};
+fill:${({ color }) => color};
 stroke:${({ stroke }) => stroke};
 stroke-width: ${({ stroke, strokeWidth }) => (stroke) && (strokeWidth? strokeWidth : 2)};
 
