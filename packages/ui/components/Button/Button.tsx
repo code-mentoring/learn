97b00c4573818c8cd0aff46b52673456a95f212e
Quim Granados <quimgranados@gmail.com>
2020-06-20 21:01:26 +1000

    feat: button component update

--- packages/ui/components/Button/Button.tsx
+++ packages/ui/components/Button/Button.tsx
@@ -1,19 +1,25 @@
 import React, { HTMLAttributes } from 'react';
 import styled, { css } from 'styled-components';
 import { Icon } from '@codement/ui';
+import { theme as th } from '../../css/theme';
 
-enum BtnType {
+export enum BtnSize {
+  small = 'small',
+  large = 'large'
+}
+
+export enum BtnType {
   primary = 'primary',
   secondary = 'secondary',
   tertiary = 'tertiary',
   transparent = 'transparent'
 }
 
-export interface ButtonProps extends HTMLAttributes<any> {
+export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
   text?: boolean;
   btnType?: 'primary' | 'secondary' | 'tertiary' | 'transparent'
   disabled?: boolean;
-  size?: string;
+  size?: 'small' | 'large';
   icon?: string;
   iconPosition?: 'left' | 'right'
   className?: string;
@@ -23,72 +29,61 @@ export const Button: React.FC<ButtonProps> = ({
   icon,
   iconPosition = 'left',
   children,
+  btnType = 'primary',
   ...props
-}) => (
-
-  <StyledButton
-    {...props}
-  >
-    { icon && iconPosition === 'left' && <ButtonIconLeft size={8} icon={icon} />}
-    { children }
-    { icon && iconPosition === 'right' && <ButtonIconRight size={8} icon={icon} />}
-  </StyledButton>
-);
+}) => <StyledButton
+  backgroundColor={btnType === 'transparent' ? th.colors[btnType] : th.colors[btnType]['500']}
+  backgroundColorHover={btnType === 'transparent' ? th.colors[btnType] : th.colors[btnType]['400']}
+  backgroundColorFocus={btnType === 'transparent' ? th.colors[btnType] : th.colors[btnType]['600']}
+  {...props}
+>
+  { icon && iconPosition === 'left' && <ButtonIconLeft size={8} icon={icon} />}
+  <span>{ children }</span>
+  { icon && iconPosition === 'right' && <ButtonIconRight size={8} icon={icon} />}
+</StyledButton>;
 
 const ButtonIconLeft = styled(props => <Icon {...props} />)`
-  margin-right: 1.4rem;
-  display: inline;
+  margin-right: 10px;
 `;
 
 const ButtonIconRight = styled(props => <Icon {...props} />)`
-  margin-left: 1.4rem;
-  display: inline;
+  margin-left: 10px;
 `;
 
-const StyledButton = styled.button<ButtonProps>`
-  ${({ size }) => size === 'large' && css`
-    height: 4.8rem;
-    line-height: 4.8rem;
-    font-size: 1.6rem;
-  `}
+export interface StyledButtonProps extends ButtonProps {
+  backgroundColor: string;
+  backgroundColorHover: string;
+  backgroundColorFocus: string;
+}
 
-  ${({ btnType }) => btnType === BtnType.secondary && css`
-    background-color: ${props => props.theme.colors.secondary['500']};
-    &:hover {
-      background-color: ${props => props.theme.colors.secondary['400']};
-    }
-    &:focus {
-    background-color: ${props => props.theme.colors.secondary['600']};
-    }
-  `}
+const StyledButton = styled.button<StyledButtonProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 800;
+  font-size: 14px;
+  height: 36px;
+  line-height: 19px;
+  padding: 13px 20px;
+  border-radius: 8px;
+  letter-spacing: 0.75px;
+  text-align: center;
+  text-transform: uppercase;
+  cursor: ${({ disabled }) => disabled && 'not-allowed'};
+  color: ${({ btnType, theme }) => (btnType === 'transparent' ? theme.colors.primary['500'] : theme.colors.white)};
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  opacity: ${({ disabled }) => disabled && '0.5'};
+  &:hover {
+    background-color: ${({ backgroundColorHover }) => backgroundColorHover};
+  }
+  &:focus {
+    background-color: ${({ backgroundColorFocus }) => backgroundColorFocus};
+    color: ${({ btnType, theme }) => btnType === 'transparent' && theme.colors.grey['500']};
+  }
 
-  ${({ btnType }) => btnType === BtnType.tertiary && css`
-    background-color: ${props => props.theme.colors.tertiary['500']};
-    &:hover {
-      background-color: ${props => props.theme.colors.tertiary['400']};
-    }
-    &:focus {
-    background-color: ${props => props.theme.colors.tertiary['600']};
-    }
-  `}
-
-  ${({ btnType }) => btnType === BtnType.transparent && css`
-    color: ${props => props.theme.colors.primary['500']};
-    background-color: ${props => props.theme.colors.transparent};
-    &:hover {
-      background-color: ${props => props.theme.colors.transparent};
-    }
-    &:focus {
-    background-color: ${props => props.theme.colors.transparent};
-    color: ${props => props.theme.colors.grey['500']};
-    }
-  `}
-
-  ${({ disabled }) => disabled && css`
-    cursor: not-allowed;
-    background-color: ${props => props.theme.colors.grey['300']};
-    &:hover {
-      background-color: ${props => props.theme.colors.grey['300']};
-    }
+  ${({ size }) => size === BtnSize.large && css`
+    height: 48px;
+    line-height: 22px;
+    font-size: 16px;
   `}
 `;
