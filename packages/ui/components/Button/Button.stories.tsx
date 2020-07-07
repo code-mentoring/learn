16b6626cf9e4b95a3f46a95adddf1872b2ac185c
Tristan Matthias <hello@tristanmatthias.com>
2020-07-07 18:41:37 -0400

    feat(ui): added initial storybook

---
+++ packages/ui/components/Button/Button.stories.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Button } from './Button';
+
+export default {
+  title: 'Buttons',
+  parameters: {
+    info: { inline: true }
+  }
+};
+
+export const ButtonCell = () => (
+  <Button>Test</Button>
+);
