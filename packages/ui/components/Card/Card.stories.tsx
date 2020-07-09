d1001de1860c454c83b6c44ba247256a553c0f15
leeyang2019 <nyang@madisoncollege.edu>
2020-07-09 13:23:32 -0400

    feat(ui): created box button and card components to storybook

---
+++ packages/ui/components/Card/Card.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card } from './Card';
+
+export default {
+  title: 'Card',
+  parameters: {
+    info: { inline: true }
+  }
+};
+
+
+export const Default = () => <Card>Test</Card>;
