57a80b3f778c399fec643972eba8a7d94c253046
leeyang2019 <nyang@madisoncollege.edu>
2020-07-12 12:20:45 -0400

    feat(ui): added all icon components to storybook

---
+++ packages/ui/components/Grid/Grid.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Grid } from './Grid';
+
+export default {
+  title: 'Grid',
+  parameters: {
+    info: { inline: true }
+  }
+};
+
+export const Default = () => <Grid />;
---
+++ packages/ui/components/Icon/icons/solid/solid.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { IconArrowDown } from './cis-arrow-down';
+import { IconCheck } from './cis-check';
+import { IconCheckCircle } from './cis-check-circle';
+import { IconChevronBottomAlt } from './cis-chevron-bottom-alt';
+import { IconChevronTopAlt } from './cis-chevron-top-alt';
+import { IconCloudUpload } from './cis-cloud-upload';
+import { IconDashboard } from './cis-dashboard';
+import { IconExclamation } from './cis-exclamation';
+import { IconEyeSlash } from './cis-eye-slash';
+import { IconFire } from './cis-fire';
+import { IconLockMinus } from './cis-lock-minus';
+import { IconPlus } from './cis-plus';
+import { IconSearch } from './cis-search';
+import { IconSettings } from './cis-settings';
+import { IconShieldAlt } from './cis-shield-alt';
+import { IconUser } from './cis-user';
+import { IconX } from './cis-x';
+
+export default {
+  title: 'Solid Icons',
+  parameters: {
+    info: { inline: true }
+  }
+};
+
+export const DownArrow = () => <IconArrowDown />;
+export const CheckIcon = () => <IconCheck />;
+export const CircleCheckIcon = () => <IconCheckCircle />;
+export const ChevronBottomAltIcon = () => <IconChevronBottomAlt />;
+export const ChevronTopAltIcon = () => <IconChevronTopAlt />;
+export const CloudUploadIcon = () => <IconCloudUpload />;
+export const DashboardIcon = () => <IconDashboard />;
+export const ExclamationIcon = () => <IconExclamation />;
+export const EyeSlashIcon = () => <IconEyeSlash />;
+export const FireIcon = () => <IconFire />;
+export const LockMinusIcon = () => <IconLockMinus />;
+export const PlusIcon = () => <IconPlus />;
+export const SearchIcon = () => <IconSearch />;
+export const SettingsIcon = () => <IconSettings />;
+export const ShieldAltIcon = () => <IconShieldAlt />;
+export const UserIcon = () => <IconUser />;
+export const XIcon = () => <IconX />;
