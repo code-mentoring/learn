8fb421dbb3de8ff82f88a514d8eb6b8f58d12f02
leeyang2019 <nyang@madisoncollege.edu>
2020-07-14 17:39:24 -0400

    feat(ui): reordered and updated Icons components

---
+++ packages/ui/components/Icon/Icon.stories.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Icon } from './Icon';
+
+export default {
+  title: 'Icons',
+  parameters: {
+    info: { inline: true }
+  }
+};
+
+
+export const ArrowDownIcon = () => <Icon icon="arrowDown" color="primary" size="xl" />;
+export const CheckIcon = () => <Icon icon="check" color="primary" size="xl" />;
+export const ChevronDownIcon = () => <Icon icon="chevronDown" color="primary" size="xl" />;
+export const ChevronUpIcon = () => <Icon icon="chevronUp" color="primary" size="xl" />;
+export const DashboardIcon = () => <Icon icon="dashboard" color="primary" size="xl" />;
+export const ErrorIcon = () => <Icon icon="error" color="primary" size="xl" />;
+export const ExclamationIcon = () => <Icon icon="exclamation" color="primary" size="xl" />;
+export const FireIcon = () => <Icon icon="fire" color="primary" size="xl" />;
+export const ImagesIcon = () => <Icon icon="images" color="primary" size="xl" />;
+export const LockIcon = () => <Icon icon="lock" color="primary" size="xl" />;
+export const LogoutIcon = () => <Icon icon="logout" color="primary" size="xl" />;
+export const MenuIcon = () => <Icon icon="menu" color="primary" size="xl" />;
+export const MinusIcon = () => <Icon icon="minus" color="primary" size="xl" />;
+export const NoteIcon = () => <Icon icon="note" color="primary" size="xl" />;
+export const PageIcon = () => <Icon icon="page" color="primary" size="xl" />;
+export const PasswordIcon = () => <Icon icon="password" color="primary" size="xl" />;
+export const PasswordEyeIcon = () => <Icon icon="passwordEye" color="primary" size="xl" />;
+export const SearchIcon = () => <Icon icon="search" color="primary" size="xl" />;
+export const SettingsIcon = () => <Icon icon="settings" color="primary" size="xl" />;
+export const ShieldAltIcon = () => <Icon icon="shieldAlt" color="primary" size="xl" />;
+export const TrashIcon = () => <Icon icon="trash" color="primary" size="xl" />;
+export const UploadIcon = () => <Icon icon="upload" color="primary" size="xl" />;
+export const uploadDuotoneIcone = () => <Icon icon="uploadDuotone" color="primary" size="xl" />;
+export const UserIcon = () => <Icon icon="user" color="primary" size="xl" />;
+export const XIcon = () => <Icon icon="x" color="primary" size="xl" />;
+export const CheckCircleIcone = () => <Icon icon="checkCircle" color="primary" size="xl" />;
