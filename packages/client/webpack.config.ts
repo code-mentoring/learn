6a095d6ed2d0758894680e082fe1c6ebb782abc8
Tristan Matthias <hello@tristanmatthias.com>
2020-07-24 00:28:41 -0500

    fix(client): config API_HOST

--- packages/client/webpack.config.ts
+++ packages/client/webpack.config.ts
@@ -58,7 +58,7 @@ const config: Configuration = {
     }),
     new ReplacePlugin({
       values: {
-        '%%API_HOST%%': isProd ? 'https://api.codementoring.co' : 'http://localhost:4000',
+        '%%API_HOST%%': process.env.API_HOST || 'http://localhost:4000',
         '%%IS_PROD%%': isProd
       }
     }),
