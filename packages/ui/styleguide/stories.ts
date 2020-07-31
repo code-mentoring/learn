50c31aeb3674d40fc701d3e2634c819fd1b9214b
Ellie-2020 <>
2020-07-30 23:09:48 -0400

    feat(ui): add text component style guide

--- packages/ui/styleguide/stories.ts
+++ packages/ui/styleguide/stories.ts
@@ -1,5 +1,6 @@
 import StoryButtons from '../components/Button/Button.story.mdx';
 import StoryIcons from '../components/Icon/Icon.story.mdx';
+import StoryText from '../components/Text/Text.story.mdx';
 
 export interface Story {
   id: string;
@@ -9,5 +10,6 @@ export interface Story {
 
 export const stories: Story[] = [
   { id: 'buttons', title: 'Buttons', story: StoryButtons },
-  { id: 'icons', title: 'Icons', story: StoryIcons }
+  { id: 'icons', title: 'Icons', story: StoryIcons },
+  { id: 'text', title: 'Text', story: StoryText }
 ];
