29b78daaff99aeb942f44aaefc36bdf24b5291fb
Suzy <suzy.nakayama@gmail.com>
2020-08-07 12:59:54 -0400

    feat(ui): StyleGuide - box - card

--- packages/ui/styleguide/stories.ts
+++ packages/ui/styleguide/stories.ts
@@ -1,5 +1,7 @@
 import StoryButtons from '../components/Button/Button.story.mdx';
 import StoryIcons from '../components/Icon/Icon.story.mdx';
+import StoryBox from '../components/Box/Box.story.mdx';
+import StoryCard from '../components/Card/Card.story.mdx';
 
 export interface Story {
   id: string;
@@ -9,5 +11,7 @@ export interface Story {
 
 export const stories: Story[] = [
   { id: 'buttons', title: 'Buttons', story: StoryButtons },
-  { id: 'icons', title: 'Icons', story: StoryIcons }
+  { id: 'icons', title: 'Icons', story: StoryIcons },
+  { id: 'box', title: 'Box', story: StoryBox },
+  { id: 'card', title: 'Card', story: StoryCard }
 ];
