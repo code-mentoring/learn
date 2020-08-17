e71424b3a0514b8eeb8749fe7313c17ac111758f
Waqas D <wdaimee@gmail.com>
2020-08-17 13:45:31 -0400

    feat(ui): add loader to style guide

--- packages/ui/styleguide/stories.ts
+++ packages/ui/styleguide/stories.ts
@@ -3,6 +3,7 @@ import StoryIcons from '../components/Icon/Icon.story.mdx';
 import StorySliderField from '../components/SliderField/SliderField.story.mdx';
 import StoryErrorMessage from '../components/ErrorMessage/ErrorMessage.story.mdx';
 import StoryText from '../components/Text/Text.story.mdx';
+import StoryLoader from '../components/Loader/Loader.story.mdx';
 
 export interface Story {
   id: string;
@@ -15,5 +16,6 @@ export const stories: Story[] = [
   { id: 'icons', title: 'Icons', story: StoryIcons },
   { id: 'sliderField', title: 'SliderField', story: StorySliderField },
   { id: 'error-message', title: 'Error Message', story: StoryErrorMessage },
-  { id: 'text', title: 'Text', story: StoryText }
+  { id: 'text', title: 'Text', story: StoryText },
+  { id: 'loader', title: 'Loader', story: StoryLoader }
 ];
